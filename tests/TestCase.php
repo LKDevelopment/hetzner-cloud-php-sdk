01d9ca652370ac9f02c3aa67341aeb382b9da46d
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-07-20 09:58:09 +0200

    Use the new api mock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,6 +26,6 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
      */
     public function setUp()
     {
-        $this->hetznerApi = new HetznerAPIClient('abcdef', 'http://localhost:4000');
+        $this->hetznerApi = new HetznerAPIClient('abcdef', 'http://localhost:8080');
     }
 }
