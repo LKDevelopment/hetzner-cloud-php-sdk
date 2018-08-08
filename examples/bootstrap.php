4578a81a158f0a08cd1e5d21bcdae75678ed342a
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2018-08-08 15:09:11 +0200

    Update whole structure

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
-$apiKey = 'ggI0ugb0UTzKxszLzl6KwuydkzJ1eM9Pp3gqQeLP0ZQEfhEZs8oLjxDmYtlLHdQJ';
+$apiKey = '{InsertApiTokenHere}';
 
 $hetznerClient = new \LKDev\HetznerCloud\HetznerAPIClient($apiKey);
