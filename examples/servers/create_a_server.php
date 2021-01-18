6417adb608c5fa1382ddb2df54272f96725c6916
Lukas Kämmerling <github@lukas-kaemmerling.de>
2021-01-18 15:20:24 +0100

    Fix sample

--- examples/servers/create_a_server.php
+++ examples/servers/create_a_server.php
@@ -8,11 +8,16 @@ $image = $hetznerClient->images()->getByName('ubuntu-20.04');
 $apiResponse = $hetznerClient->servers()->createInLocation('my-example-server.test', $serverType, $image, $location);
 $server = $apiResponse->getResponsePart('server');
 $action = $apiResponse->getResponsePart('action');
+$next_actions = $apiResponse->getResponsePart('next_actions');
 echo 'Server: '.$server->name.PHP_EOL;
 echo 'IP: '.$server->publicNet->ipv4->ip.PHP_EOL;
 echo 'Password: '.$apiResponse->getResponsePart('root_password').PHP_EOL;
 echo 'Now we wait on the success of the server creation!'.PHP_EOL;
 echo date('H:i:s').PHP_EOL;
 $action->waitUntilCompleted();
+
+foreach ($next_actions as $na) {
+  $na->waitUntilCompleted();
+}
 echo date('H:i:s').PHP_EOL;
 echo 'Done!';
