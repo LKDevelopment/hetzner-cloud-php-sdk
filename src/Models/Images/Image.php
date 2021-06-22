330cf31df25068fbe6ecedeb831b06905c7882c5
Lukas Kämmerling <github@lukas-kaemmerling.de>
2021-06-22 06:52:14 +0200

    Fix style

--- src/Models/Images/Image.php
+++ src/Models/Images/Image.php
@@ -224,7 +224,7 @@ class Image extends Model implements Resource
     {
         return HetznerAPIClient::$instance->images()->get($this->id);
     }
-    
+
     public function getCreatedDateTime()
     {
         return new \DateTime($this->created);
