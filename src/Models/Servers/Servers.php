d81f24801770c4e137d13ae68b95623138c6ebd9
Christian Linde <info@christian-linde.de>
2022-12-15 08:38:50 +0100

    add missing public_net, placement_group in server create action

--- src/Models/Servers/Servers.php
+++ src/Models/Servers/Servers.php
@@ -131,6 +131,7 @@ class Servers extends Model
      * @param  array  $networks
      * @param  array  $labels
      * @param  array  $firewalls
+     * @param  array  $public_net
      * @return APIResponse|null
      *
      * @throws \LKDev\HetznerCloud\APIException
@@ -147,7 +148,9 @@ class Servers extends Model
         $automount = false,
         $networks = [],
         array $labels = [],
-        array $firewalls = []
+        array $firewalls = [],
+        array $public_net = [],
+        ?int $placement_group = null
     ): ?APIResponse {
         $parameters = [
             'name' => $name,
@@ -160,12 +163,16 @@ class Servers extends Model
             'volumes' => $volumes,
             'automount' => $automount,
             'networks' => $networks,
+            'public_net' => $public_net
         ];
         if (! empty($labels)) {
             $parameters['labels'] = $labels;
         }
         if (! empty($firewalls)) {
             $parameters['firewalls'] = json_encode($firewalls);
+        }
+        if($placement_group != null) {
+            $parameters['placement_group'] = $placement_group;
         }
         $response = $this->httpClient->post('servers', [
             'json' => $parameters,
@@ -219,7 +226,9 @@ class Servers extends Model
                                      bool $automount = false,
                                      array $networks = [],
                                      array $labels = [],
-                                     array $firewalls = []
+                                     array $firewalls = [],
+                                     array $public_net = [],
+                                     ?int $placement_group = null
     ): ?APIResponse {
         $parameters = [
             'name' => $name,
@@ -232,12 +241,16 @@ class Servers extends Model
             'volumes' => $volumes,
             'automount' => $automount,
             'networks' => $networks,
+            'public_net' => $public_net
         ];
         if (! empty($labels)) {
             $parameters['labels'] = $labels;
         }
         if (! empty($firewalls)) {
             $parameters['firewalls'] = json_encode($firewalls);
+        }
+        if($placement_group != null) {
+            $parameters['placement_group'] = $placement_group;
         }
         $response = $this->httpClient->post('servers', [
             'json' => $parameters,
