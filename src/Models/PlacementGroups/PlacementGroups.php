672ec91f10074bb8ba99d5ad722638ef3c500d32
Christian Linde <info@christian-linde.de>
2023-09-11 09:43:39 +0200

    add placementgroups

---
+++ src/Models/PlacementGroups/PlacementGroups.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace LKDev\HetznerCloud\Models\PlacementGroups;
+
+use LKDev\HetznerCloud\APIResponse;
+use LKDev\HetznerCloud\HetznerAPIClient;
+use LKDev\HetznerCloud\Models\Contracts\Resources;
+use LKDev\HetznerCloud\Models\Meta;
+use LKDev\HetznerCloud\Models\Model;
+use LKDev\HetznerCloud\Models\PlacementGroups\PlacementGroupRequestOpts;
+use LKDev\HetznerCloud\RequestOpts;
+use LKDev\HetznerCloud\Traits\GetFunctionTrait;
+
+class PlacementGroups extends Model implements Resources {
+
+    use GetFunctionTrait;
+
+    /**
+     * @var array
+     */
+    protected $placement_groups;
+
+    /**
+     * Returns all existing placementGroup objects.
+     *
+     * @see https://docs.hetzner.cloud/#placement-groups-get-all-placementgroups
+     *
+     * @param  RequestOpts|null  $requestOpts
+     * @return array
+     *
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function all(RequestOpts $requestOpts = null): array
+    {
+        if ($requestOpts == null) {
+            $requestOpts = new PlacementGroupRequestOpts();
+        }
+
+        return $this->_all($requestOpts);
+    }
+
+    /**
+     * Returns all existing PlacementGroup objects.
+     *
+     * @see https://docs.hetzner.cloud/#placement-groups-get-all-placementgroups
+     *
+     * @param  RequestOpts|null  $requestOpts
+     * @return APIResponse|null
+     *
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function list(RequestOpts $requestOpts = null): ?APIResponse
+    {
+        if ($requestOpts == null) {
+            $requestOpts = new PlacementGroupRequestOpts();
+        }
+        $response = $this->httpClient->get('placement_groups'.$requestOpts->buildQuery());
+        if (! HetznerAPIClient::hasError($response)) {
+            $resp = json_decode((string) $response->getBody());
+
+            return APIResponse::create([
+                'meta' => Meta::parse($resp->meta),
+                $this->_getKeys()['many' ]=> self::parse($resp->{$this->_getKeys()['many']})->{$this->_getKeys()['many']},
+            ], $response->getHeaders());
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns a specific PlacementGroup object. The PlacementGroup must exist inside the project.
+     *
+     * @see https://docs.hetzner.cloud/#placement-groups-get-a-placementgroup
+     *
+     * @param  int  $serverId
+     * @return PlacementGroup
+     *
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function getById(int $serverId): ?PlacementGroup
+    {
+        $response = $this->httpClient->get('placement_group/'.$serverId);
+        if (! HetznerAPIClient::hasError($response)) {
+            return PlacementGroup::parse(json_decode((string) $response->getBody())->network);
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns a specific placementGroup object by its name. The placementGroup must exist inside the project.
+     *
+     * @see https://docs.hetzner.cloud/#placement-groups-get-all-placementgroups
+     *
+     * @param  string  $name
+     * @return PlacementGroup|null
+     *
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function getByName(string $name): ?PlacementGroup
+    {
+        $placementGroups = $this->list(new NetworkRequestOpts($name));
+
+        return (count($placementGroups->placement_groups) > 0) ? $placementGroups->placement_groups[0] : null;
+    }
+
+    /**
+     * @param  $input
+     * @return $this
+     */
+    public function setAdditionalData($input)
+    {
+        $this->placement_groups = collect($input)
+            ->map(function ($placementGroup) {
+                if ($placementGroup != null) {
+                    return PlacementGroup::parse($placementGroup);
+                }
+            })
+            ->toArray();
+
+        return $this;
+    }
+
+    /**
+     * @param  string  $name
+     * @param  string  $type
+     * @param  array  $labels
+     */
+    public function create(string $name, string $type, array $labels = [])
+    {
+        $payload = [
+            'name' => $name,
+            'type' => $type,
+        ];
+        if (! empty($labels)) {
+            $payload['labels'] = $labels;
+        }
+
+        $response = $this->httpClient->post('placement_groups', [
+            'json' => $payload,
+        ]);
+        if (! HetznerAPIClient::hasError($response)) {
+            $payload = json_decode((string) $response->getBody());
+
+            return APIResponse::create([
+                'placement_group' => PlacementGroup::parse($payload->placement_group),
+            ], $response->getHeaders());
+        }
+    }
+
+    /**
+     * @param  $input
+     * @return static
+     */
+    public static function parse($input)
+    {
+        return (new self())->setAdditionalData($input);
+    }
+
+    /**
+     * @return array
+     */
+    public function _getKeys(): array
+    {
+        return ['one' => 'placementgroup', 'many' => 'placementgroups'];
+    }
+
+}
---
+++ tests/Unit/Models/PlacementGroups/PlacementGroupsTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace LKDev\Tests\Unit\Models\PlacementGroups;
+
+use GuzzleHttp\Psr7\Response;
+use LKDev\HetznerCloud\APIResponse;
+use LKDev\HetznerCloud\Models\Networks\Network;
+use LKDev\HetznerCloud\Models\Networks\Networks;
+use LKDev\HetznerCloud\Models\Networks\Route;
+use LKDev\HetznerCloud\Models\Networks\Subnet;
+use LKDev\HetznerCloud\Models\PlacementGroups\PlacementGroup;
+use LKDev\HetznerCloud\Models\PlacementGroups\PlacementGroups;
+use LKDev\HetznerCloud\Models\Protection;
+use LKDev\HetznerCloud\Models\Servers\Server;
+use LKDev\Tests\TestCase;
+
+/**
+ * Class NetworksTest.
+ */
+class PlacementGroupsTest extends TestCase
+{
+    /**
+     * @var PlacementGroups
+     */
+    protected $placement_groups;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->placement_groups = new PlacementGroups($this->hetznerApi->getHttpClient());
+    }
+
+    /**
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function testAll()
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/placement_groups.json')));
+        $placement_groups = $this->placement_groups->all();
+        $this->assertCount(1, $placement_groups);
+
+        $this->assertLastRequestEquals('GET', '/placement_groups');
+    }
+
+    /**
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function testList()
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/placement_groups.json')));
+        $placement_groups = $this->placement_groups->list()->placement_groups;
+        $this->assertCount(1, $placement_groups);
+        $this->assertLastRequestEquals('GET', '/placement_groups');
+    }
+
+    /**
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function testGetByName()
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/placement_groups.json')));
+        $placement_group = $this->placement_groups->getByName('my_placemengroup');
+        $this->assertEquals(4711, $placement_group->id);
+        $this->assertEquals('my_placemengroup', $placement_group->name);
+
+        $this->assertCount(1, $placement_group->servers);
+        $this->assertInstanceOf(Server::class, $placement_group->servers[0]);
+
+        $this->assertEmpty($placement_group->labels);
+
+        $this->assertLastRequestEquals('GET', '/placement_groups');
+    }
+
+    /**
+     * @throws \LKDev\HetznerCloud\APIException
+     */
+    public function testGet()
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/placement_group.json')));
+        $placement_group = $this->placement_groups->get(4711);
+        $this->assertEquals(4711, $placement_group->id);
+        $this->assertEquals('my_placemengroup', $placement_group->name);
+
+        $this->assertCount(1, $placement_group->servers);
+        $this->assertInstanceOf(Server::class, $placement_group->servers[0]);
+
+        $this->assertEmpty($placement_group->labels);
+
+        $this->assertLastRequestEquals('GET', '/placement_groups/4711');
+    }
+
+    public function testBasicCreate()
+    {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__.'/fixtures/placement_group.json')));
+        $resp = $this->placement_groups->create('my_placemengroup', "spread", []);
+        $this->assertInstanceOf(APIResponse::class, $resp);
+        $this->assertInstanceOf(PlacementGroup::class, $resp->getResponsePart('placement_group'));
+
+        $this->assertLastRequestEquals('POST', '/placement_groups');
+        $this->assertLastRequestBodyParametersEqual(['name' => 'my_placemengroup', 'type' => 'spread']);
+    }
+
+}
