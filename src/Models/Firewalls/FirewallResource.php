00c0f0065238a97273e9ecdec8332353e8da81e8
Bashar Bhuiyan <13801112+bashar94@users.noreply.github.com>
2024-03-05 11:51:00 +0600

    Update FirewallResource.php
    The firewallResource class has 2 types, only the 'server' type was included previously, adding the 'label_selector' type

--- src/Models/Firewalls/FirewallResource.php
+++ src/Models/Firewalls/FirewallResource.php
@@ -10,6 +10,7 @@ use LKDev\HetznerCloud\Models\Servers\Server;
 class FirewallResource
 {
     const TYPE_SERVER = 'server';
+    const TYPE_LABEL_SELECTOR = 'label_selector';
 
     /**
      * @var string
@@ -20,26 +21,37 @@ class FirewallResource
      */
     public $server;
 
+     /**
+     * @var string|null
+     */
+    public $selector;
+
     /**
      * FirewallResource constructor.
      *
-     * @param  string  $type
-     * @param  Server|null  $server
+     * @param string $type
+     * @param Server|null $server
+     * @param string|null $selector
      */
-    public function __construct(string $type, ?Server $server)
+    public function __construct(string $type, ?Server $server = null, ?string $selector = null)
     {
         $this->type = $type;
         $this->server = $server;
+        $this->selector = $selector;
     }
 
+
     /**
-     * @return string[]
+     * @return array
      */
     public function toRequestSchema(): array
     {
         $s = ['type' => $this->type];
-        if ($this->type == self::TYPE_SERVER) {
+
+        if ($this->type == self::TYPE_SERVER && $this->server !== null) {
             $s['server'] = ['id' => $this->server->id];
+        } else if ($this->type == self::TYPE_LABEL_SELECTOR && $this->selector !== null) {
+            $s['label_selector'] = ['selector' => $this->selector];
         }
 
         return $s;
