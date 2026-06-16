49502e6de49487884da3049d310bd41291c48639
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2026-06-16 14:17:32 +0200

    Add support for optional `public_net` parameter in server creation methods and add corresponding unit tests

--- tests/Unit/Models/Servers/ServersTest.php
+++ tests/Unit/Models/Servers/ServersTest.php
@@ -10,7 +10,11 @@
 namespace LKDev\Tests\Integration\Servers;
 
 use GuzzleHttp\Psr7\Response;
+use LKDev\HetznerCloud\Models\Datacenters\Datacenter;
+use LKDev\HetznerCloud\Models\Images\Image;
+use LKDev\HetznerCloud\Models\Locations\Location;
 use LKDev\HetznerCloud\Models\Servers\Servers;
+use LKDev\HetznerCloud\Models\Servers\Types\ServerType;
 use LKDev\Tests\TestCase;
 
 class ServersTest extends TestCase
@@ -67,5 +71,45 @@ class ServersTest extends TestCase
         $this->assertEquals($servers[0]->id, 42);
         $this->assertEquals($servers[0]->name, 'my-server');
         $this->assertLastRequestEquals('GET', '/servers');
+    }
+
+    public function testCreateInLocationOmitsPublicNetByDefault()
+    {
+        $this->mockHandler->append(new Response(201, [], file_get_contents(__DIR__.'/fixtures/server_create.json')));
+        $this->servers->createInLocation('my-server', new ServerType(1), new Image(4711), new Location(1, 'fsn1'));
+
+        $body = json_decode((string) $this->mockHandler->getLastRequest()->getBody(), true);
+        $this->assertArrayNotHasKey('public_net', $body);
+        $this->assertLastRequestEquals('POST', '/servers');
+    }
+
+    public function testCreateInLocationSendsPublicNetWhenProvided()
+    {
+        $this->mockHandler->append(new Response(201, [], file_get_contents(__DIR__.'/fixtures/server_create.json')));
+        $publicNet = ['enable_ipv4' => true, 'enable_ipv6' => false];
+        $this->servers->createInLocation('my-server', new ServerType(1), new Image(4711), new Location(1, 'fsn1'), public_net: $publicNet);
+
+        $this->assertLastRequestEquals('POST', '/servers');
+        $this->assertLastRequestBodyParametersEqual(['public_net' => $publicNet]);
+    }
+
+    public function testCreateInDatacenterOmitsPublicNetByDefault()
+    {
+        $this->mockHandler->append(new Response(201, [], file_get_contents(__DIR__.'/fixtures/server_create.json')));
+        $this->servers->createInDatacenter('my-server', new ServerType(1), new Image(4711), new Datacenter(1, 'fsn1-dc8', 'Falkenstein 1 DC 8', new Location(1, 'fsn1')));
+
+        $body = json_decode((string) $this->mockHandler->getLastRequest()->getBody(), true);
+        $this->assertArrayNotHasKey('public_net', $body);
+        $this->assertLastRequestEquals('POST', '/servers');
+    }
+
+    public function testCreateInDatacenterSendsPublicNetWhenProvided()
+    {
+        $this->mockHandler->append(new Response(201, [], file_get_contents(__DIR__.'/fixtures/server_create.json')));
+        $publicNet = ['enable_ipv4' => true, 'enable_ipv6' => false];
+        $this->servers->createInDatacenter('my-server', new ServerType(1), new Image(4711), new Datacenter(1, 'fsn1-dc8', 'Falkenstein 1 DC 8', new Location(1, 'fsn1')), public_net: $publicNet);
+
+        $this->assertLastRequestEquals('POST', '/servers');
+        $this->assertLastRequestBodyParametersEqual(['public_net' => $publicNet]);
     }
 }
